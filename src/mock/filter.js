efc8a7595a3a20f35636379a360d055418ea2aab
Elizaveta-Bakulina <bakulina.e@sales.saturn.net>
2024-10-21 17:50:34 +0300

    Удаление моковых данных

--- src/mock/filter.js
+++
@@ -1,8 +0,0 @@
-import {filter} from '../utils/filter.js';
-
-const generateFilter = (routePoints) => Object.entries(filter).map(([filterType, filterRoutePoints])=>({
-  type:filterType,
-  count:filterRoutePoints(routePoints).length
-}));
-
-export {generateFilter};

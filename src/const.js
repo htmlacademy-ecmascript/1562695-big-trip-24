eebcd22b376f1898ed28d33f15365f385c345564
Elizaveta-Bakulina <148747084+Elizaveta-Bakulina@users.noreply.github.com>
2024-10-21 20:39:04 +0300

    Удаление неиспользуемых переменных

--- src/const.js
+++ src/const.js
@@ -1,8 +1,5 @@
 const TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
-const CITIES = ['Amsterdam','Paris', 'London', 'Madrid', 'Lissabon', 'Rome', 'Helsinki' ];
-const DESCRIPTION_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
 
-const POINT_COUNT = 3;
 const MAX_COUNT_DESTINATIONS = 3;
 const DEFAULT_TYPE = 'flight';
 const BlankPoint = {
@@ -68,4 +65,4 @@ const TimeLimit = {
   UPPER_LIMIT: 1000,
 };
 
-export {TYPES, CITIES, DESCRIPTION_TEXT, POINT_COUNT, DEFAULT_TYPE, BlankPoint, DateFormat, EmptyListText, SortType, FilterType, Mode, DISABLED_SORT_TYPES, UserAction, UpdateType, ValidationStyle, TimeLimit, MAX_COUNT_DESTINATIONS};
+export {TYPES, DEFAULT_TYPE, BlankPoint, DateFormat, EmptyListText, SortType, FilterType, Mode, DISABLED_SORT_TYPES, UserAction, UpdateType, ValidationStyle, TimeLimit, MAX_COUNT_DESTINATIONS};
